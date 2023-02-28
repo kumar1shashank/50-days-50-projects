31eaf39315919957106d2210f528c6e8dfabfcf2
shashank <kumarshashank1>
2023-02-28 23:48:30 +0530

    err

---
+++ day1-expanding_Cards/script.js
@@ -0,0 +1,19 @@
+console.log("script test ");
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    
+    panel.addEventListener('click', ()=>{
+        removeActiveClass()
+        panel.classList.add('active')
+
+    })
+});
+
+function removeActiveClass() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+        
+    });
+}
